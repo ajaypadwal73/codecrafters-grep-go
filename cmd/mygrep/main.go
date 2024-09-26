ec527406590f004afd4ae1363692ec5f6f5a6fa0
Ajay <padwal@Ajays-Air-11.lan>
2024-09-27 01:29:06 +0530

    codecrafters submit [skip ci]

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
+	"unicode"
 	"unicode/utf8"
 )
 
@@ -12,7 +14,7 @@ import (
 var _ = bytes.ContainsAny
 
 // Usage: echo <input_text> | your_program.sh -E <pattern>
-func main() {
+func main2() {
 	if len(os.Args) < 3 || os.Args[1] != "-E" {
 		fmt.Fprintf(os.Stderr, "usage: mygrep -E <pattern>\n")
 		os.Exit(2) // 1 means no lines were selected, >1 means error
@@ -37,13 +39,13 @@ func main() {
 		if !ok {
 			os.Exit(1)
 		}
-	case len(pattern) > 3 && pattern[0] == '[' && pattern[len(pattern) - 1] == ']' && pattern[1] == '^':
-		ok := matchnNegativeCharGroups(line, pattern[2:len(pattern)- 1])
+	case len(pattern) > 3 && pattern[0] == '[' && pattern[len(pattern)-1] == ']' && pattern[1] == '^':
+		ok := matchnNegativeCharGroups(line, pattern[2:len(pattern)-1])
 		if !ok {
 			os.Exit(1)
-		}	
-	case len(pattern) > 2 && pattern[0] == '[' && pattern[len(pattern) - 1] == ']':
-		ok := matchPositiveCharGroups(line, pattern[1:len(pattern)- 1])
+		}
+	case len(pattern) > 2 && pattern[0] == '[' && pattern[len(pattern)-1] == ']':
+		ok := matchPositiveCharGroups(line, pattern[1:len(pattern)-1])
 		if !ok {
 			os.Exit(1)
 		}
@@ -78,7 +80,7 @@ func matchLine(line []byte, pattern string) (bool, error) {
 	return ok, nil
 }
 
-func matchDigits(line []byte) (bool) {
+func matchDigits(line []byte) bool {
 	ok := bytes.ContainsAny(line, "012345678")
 	return ok
 }
@@ -90,16 +92,160 @@ func matchAlphaNumeric(line []byte) bool {
 
 }
 
-func matchPositiveCharGroups(line []byte, pattern string) (bool) {
+func matchPositiveCharGroups(line []byte, pattern string) bool {
 	ok := bytes.ContainsAny(line, pattern)
 	return ok
 }
 
-func matchnNegativeCharGroups(line []byte, pattern string) (bool) {
+func matchnNegativeCharGroups(line []byte, pattern string) bool {
 	for _, c := range line {
 		if !bytes.ContainsAny([]byte(pattern), string(c)) {
 			return true
 		}
 	}
 	return false
+}
+
+type matcher interface {
+	match(r rune) bool
+	isLiteral() bool
+}
+
+type digitMatcher struct{}
+
+func (d digitMatcher) match(r rune) bool {
+	return unicode.IsDigit(r)
+}
+
+func (d digitMatcher) isLiteral() bool {
+	return false
+}
+
+type wordMatcher struct{}
+
+func (w wordMatcher) match(r rune) bool {
+	return unicode.IsDigit(r) || unicode.IsLetter(r) || r == '-'
+}
+
+func (w wordMatcher) isLiteral() bool {
+	return false
+}
+
+type literalMatcher struct {
+	char rune
+}
+
+func (l literalMatcher) match(r rune) bool {
+	return l.char == r
+}
+
+func (l literalMatcher) isLiteral() bool {
+	return true
+}
+
+type negativeCharGroupMatcher struct {
+	chars string
+}
+
+func (n negativeCharGroupMatcher) match(r rune) bool {
+	return !strings.ContainsRune(n.chars, r)
+}
+
+func (n negativeCharGroupMatcher) isLiteral() bool {
+	return false
+}
+
+type positiveCharGroupMatcher struct {
+	chars string
+}
+
+func (n positiveCharGroupMatcher) match(r rune) bool {
+	return strings.ContainsRune(n.chars, r)
+}
+
+func (n positiveCharGroupMatcher) isLiteral() bool {
+	return false
+}
+
+func parsePattern(pattern string) []matcher {
+	var matchers []matcher
+	for i := 0; i < len(pattern); i++ {
+
+		if pattern[i] == '\\' && i + 1 < len(pattern) {
+			switch pattern[i+1] {
+			case 'd':
+				matchers = append(matchers, digitMatcher{})
+			case 'w':
+				matchers = append(matchers, wordMatcher{})	
+			default:
+				matchers = append(matchers, literalMatcher{char: rune(pattern[i+1])})
+			}
+		} else if pattern[i] == '[' && i + 1 < len(pattern) {
+			end := strings.IndexByte(pattern[i:], ']')
+
+			if end == -1 {
+				matchers = append(matchers, literalMatcher{char: rune(pattern[i])})
+			} else {
+				if pattern[i+1] == '^' {
+					matchers = append(matchers, negativeCharGroupMatcher{chars: pattern[i+2:i+end]})
+				} else {
+					// postive char group
+					matchers = append(matchers, positiveCharGroupMatcher{chars: pattern[i+1:i+end]})
+				}
+				i += end	
+			}
+		} else {
+			matchers = append(matchers, literalMatcher{char: rune(pattern[i])})
+		}
+	}
+	return matchers
+}
+
+
+func matchPattern(input string, pattern string) bool {
+	matchers := parsePattern(pattern)
+	
+	for startIdx := 0; startIdx < len(input); startIdx++ {
+		inputIdx, matcherIdx := startIdx, 0
+
+		for inputIdx < len(input) && matcherIdx < len(matchers) {
+			if matchers[matcherIdx].match(rune(input[inputIdx])) {
+				inputIdx++
+				matcherIdx++
+			} else if matcherIdx > 0 && !matchers[matcherIdx-1].isLiteral() {
+				// If the previous matcher was non-literal (like \d or \w), we can try the next matcher
+				matcherIdx++
+			} else {
+				// If we don't match, break and try starting from the next character
+				break
+			}
+		}
+
+		if matcherIdx == len(matchers) {
+			return true // We found a complete match
+		}
+	}
+
+	return false // No match found in the entire input
+}
+
+
+func main() {
+	if len(os.Args) != 3 || os.Args[1] != "-E" {
+		fmt.Fprintf(os.Stderr, "Usage: %s -E <pattern>\n", os.Args[0])
+		os.Exit(2)
+	}
+
+	pattern := os.Args[2]
+	input, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading input: %v\n", err)
+		os.Exit(2)
+	}
+
+	if matchPattern(string(input), pattern) {
+		os.Exit(0)
+	} else {
+		os.Exit(1)
+	}
 }
