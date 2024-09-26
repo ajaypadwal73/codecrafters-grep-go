4f01c1eb20726c020bb84d03dfbebd5d5455e3df
Ajay <padwal@Ajays-Air-11.lan>
2024-09-27 01:30:11 +0530

    codecrafters submit [skip ci]

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -7,105 +7,12 @@ import (
 	"os"
 	"strings"
 	"unicode"
-	"unicode/utf8"
 )
 
 // Ensures gofmt doesn't remove the "bytes" import above (feel free to remove this!)
 var _ = bytes.ContainsAny
 
 // Usage: echo <input_text> | your_program.sh -E <pattern>
-func main2() {
-	if len(os.Args) < 3 || os.Args[1] != "-E" {
-		fmt.Fprintf(os.Stderr, "usage: mygrep -E <pattern>\n")
-		os.Exit(2) // 1 means no lines were selected, >1 means error
-	}
-
-	pattern := os.Args[2]
-
-	line, err := io.ReadAll(os.Stdin) // assume we're only dealing with a single line
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: read input text: %v\n", err)
-		os.Exit(2)
-	}
-
-	switch {
-	case pattern == "\\d":
-		ok := matchDigits(line)
-		if !ok {
-			os.Exit(1)
-		}
-	case pattern == "\\w":
-		ok := matchAlphaNumeric(line)
-		if !ok {
-			os.Exit(1)
-		}
-	case len(pattern) > 3 && pattern[0] == '[' && pattern[len(pattern)-1] == ']' && pattern[1] == '^':
-		ok := matchnNegativeCharGroups(line, pattern[2:len(pattern)-1])
-		if !ok {
-			os.Exit(1)
-		}
-	case len(pattern) > 2 && pattern[0] == '[' && pattern[len(pattern)-1] == ']':
-		ok := matchPositiveCharGroups(line, pattern[1:len(pattern)-1])
-		if !ok {
-			os.Exit(1)
-		}
-	default:
-		ok, err := matchLine(line, pattern)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "error: %v\n", err)
-			os.Exit(2)
-		}
-		if !ok {
-			os.Exit(1)
-		}
-
-	}
-
-	// default exit code is 0 which means success
-}
-
-func matchLine(line []byte, pattern string) (bool, error) {
-	if utf8.RuneCountInString(pattern) != 1 {
-		return false, fmt.Errorf("unsupported pattern: %q", pattern)
-	}
-
-	var ok bool
-
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this to pass the first stage
-	ok = bytes.ContainsAny(line, pattern)
-
-	return ok, nil
-}
-
-func matchDigits(line []byte) bool {
-	ok := bytes.ContainsAny(line, "012345678")
-	return ok
-}
-
-func matchAlphaNumeric(line []byte) bool {
-	pattern := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_"
-	ok := bytes.ContainsAny(line, pattern)
-	return ok
-
-}
-
-func matchPositiveCharGroups(line []byte, pattern string) bool {
-	ok := bytes.ContainsAny(line, pattern)
-	return ok
-}
-
-func matchnNegativeCharGroups(line []byte, pattern string) bool {
-	for _, c := range line {
-		if !bytes.ContainsAny([]byte(pattern), string(c)) {
-			return true
-		}
-	}
-	return false
-}
-
 type matcher interface {
 	match(r rune) bool
 	isLiteral() bool
@@ -171,28 +78,28 @@ func parsePattern(pattern string) []matcher {
 	var matchers []matcher
 	for i := 0; i < len(pattern); i++ {
 
-		if pattern[i] == '\\' && i + 1 < len(pattern) {
+		if pattern[i] == '\\' && i+1 < len(pattern) {
 			switch pattern[i+1] {
 			case 'd':
 				matchers = append(matchers, digitMatcher{})
 			case 'w':
-				matchers = append(matchers, wordMatcher{})	
+				matchers = append(matchers, wordMatcher{})
 			default:
 				matchers = append(matchers, literalMatcher{char: rune(pattern[i+1])})
 			}
-		} else if pattern[i] == '[' && i + 1 < len(pattern) {
+		} else if pattern[i] == '[' && i+1 < len(pattern) {
 			end := strings.IndexByte(pattern[i:], ']')
 
 			if end == -1 {
 				matchers = append(matchers, literalMatcher{char: rune(pattern[i])})
 			} else {
 				if pattern[i+1] == '^' {
-					matchers = append(matchers, negativeCharGroupMatcher{chars: pattern[i+2:i+end]})
+					matchers = append(matchers, negativeCharGroupMatcher{chars: pattern[i+2 : i+end]})
 				} else {
 					// postive char group
-					matchers = append(matchers, positiveCharGroupMatcher{chars: pattern[i+1:i+end]})
+					matchers = append(matchers, positiveCharGroupMatcher{chars: pattern[i+1 : i+end]})
 				}
-				i += end	
+				i += end
 			}
 		} else {
 			matchers = append(matchers, literalMatcher{char: rune(pattern[i])})
@@ -201,10 +108,9 @@ func parsePattern(pattern string) []matcher {
 	return matchers
 }
 
-
 func matchPattern(input string, pattern string) bool {
 	matchers := parsePattern(pattern)
-	
+
 	for startIdx := 0; startIdx < len(input); startIdx++ {
 		inputIdx, matcherIdx := startIdx, 0
 
@@ -228,7 +134,6 @@ func matchPattern(input string, pattern string) bool {
 
 	return false // No match found in the entire input
 }
-
 
 func main() {
 	if len(os.Args) != 3 || os.Args[1] != "-E" {
